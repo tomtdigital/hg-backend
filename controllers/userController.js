8bd752e3a2865778852f7eff765b3723cee9f51c
Tom Davies <tomdavies@McBk-0148.QLOCOM>
2024-01-26 16:33:28 +0000

    user info added

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,13 +39,42 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Authenticate user
+// @route POST /api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("invalid credentials");
+  }
+});
+
+// @desc Get user data
+// @route GET /api/users/me
+// @access Private
+const getMe = asyncHandler(async (req, res) => {
+  res.status(200).json(req.user);
+});
+
 // Generate JWT
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
 
 module.exports = {
-  // loginUser,
+  loginUser,
   registerUser,
-  // getMe
+  getMe,
 };
